041fc430cf6205a0afd655f4130b11e2671a9fce
youinmelin <ygqs@sina.com>
2020-02-04 15:34:07 +0800

    closure

---
+++ closure_work_01.py
@@ -0,0 +1,15 @@
+# function:y=x*k+b ,computer y with giving x,k,b
+def line_points(k,b):
+    def line_x(x):
+        # print (x * k + b)
+        return(x * k + b)
+    return line_x
+
+line = line_points(1,2)
+print(line(0))
+print(line(1))
+print(line(2))
+line = line_points(11,22)
+print(line(0))
+print(line(1))
+print(line(2))

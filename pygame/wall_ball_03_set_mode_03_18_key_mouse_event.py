02b9306b2653d14496d3eeb98b8b43b13f0f12f4
youinmelin <ygqs@sina.com>
2020-02-23 22:54:38 +0800

    pygame

--- pygame/wall_ball_03_set_mode_03_18_key_mouse_event.py
+++ pygame/wall_ball_03_set_mode_03_18_key_mouse_event.py
@@ -1,8 +1,5 @@
-# https://www.bilibili.com/video/av19574503?p=19
-# mouse event
-# when press mouse left key, the ball stop. 
-# when press mouse left key and move mouse, the ball move with the mouse
-# when release mouse left key, the ball continue to move 
+# https://www.bilibili.com/video/av19574503?p=18
+# pygame.display
 import pygame,sys
 
 pygame.init()
@@ -18,8 +15,6 @@ screen = pygame.display.set_mode((WIN_X,WIN_Y),pygame.RESIZABLE)
 ball_img = 'PYG02-ball.gif'
 speedx = 1
 speedy = 1
-# mark the ball's status
-still = False
 ball = pygame.image.load(ball_img)
 ball_rect = ball.get_rect()
 fps = 300
@@ -33,23 +28,19 @@ while True:
             WIN_X= event.w
             WIN_Y= event.h
             screen = pygame.display.set_mode((WIN_X,WIN_Y),pygame.RESIZABLE)
-        
-        if event.type == pygame.MOUSEBUTTONDOWN :
-            if event.button == 1: # left key of mouse
-                still = True
-        if event.type == pygame.MOUSEBUTTONUP:
-            still = False
-            if event.button == 1: # if release, move the ball to the mouse x y
-                ball_rect = ball_rect.move(event.pos[0] - ball_rect.left, event.pos[1] - ball_rect.top)
         if event.type == pygame.MOUSEMOTION:
-            if event.buttons[0] == 1:
-                ball_rect = ball_rect.move(event.pos[0] - ball_rect.left, event.pos[1] - ball_rect.top)
-
+            print('[MOUSEMOTION]event.pos:',event.pos,' event.rel:',event.rel,' event.buttons:',event.buttons)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print('[MOUSEDOWN]event.pos:',event.pos,' event.button:',event.button)
+        if event.type == pygame.MOUSEBUTTONUP:
+            print('[MOUSEUP]event.pos:',event.pos,' event.button:',event.button)
         if event.type == pygame.KEYDOWN:
+            if event.unicode == '':
+                print('[KEYDOWN]:','#',event.key,event.mod)
+            else:
+                print('[KEYDOWN]:',event.unicode,event.key,event.mod)
             if event.key == pygame.K_ESCAPE:
                 sys.exit()
-
-            # control speed by direction key
             if speedy > 0:
                 if event.key == pygame.K_DOWN:
                     speedy -= 1
@@ -72,20 +63,12 @@ while True:
                     speedx -= 1
     # ball_rect = rect(100,200,50,50)
     # 如果窗口没有最小化，就移动小球
-    if pygame.display.get_active() and not still:
+    if pygame.display.get_active():
         ball_rect = ball_rect.move(speedx,speedy)
     if ball_rect.left < 0 or ball_rect.right > WIN_X:
         speedx = - speedx
-        if ball_rect.left < 0:
-            ball_rect.left = 0
-        if ball_rect.right > WIN_X:
-            ball_rect.right = WIN_X
     if ball_rect.top < 0 or ball_rect.bottom > WIN_Y:
         speedy = - speedy
-        if ball_rect.top < 0:
-            ball_rect.top = 0
-        if ball_rect.bottom > WIN_Y:
-            ball_rect.bottom = WIN_Y
     # print (ball_rect)
     screen.fill((0,0,0))
     screen.blit(ball,ball_rect)

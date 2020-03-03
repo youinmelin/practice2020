d8a8d39c79ef9e7e094843ded601fa285288c5ba
youinmelin <ygqs@sina.com>
2020-03-03 16:48:26 +0800

    plane

---
+++ pygame/plane_main.py
@@ -0,0 +1,15 @@
+import pygame
+from plane_sprites import *
+
+class PlaneGame:
+
+    def __init__(self):
+        print('game initialize')
+
+
+    def start_game(self):
+        print('game start')
+
+if __name__ == '__main__':
+    game = PlaneGame()
+    game.start_game()

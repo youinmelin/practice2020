4acc2ba3fad90b37be2f10dd5482ca22f0f635df
youinmelin <ygqs@sina.com>
2020-08-29 16:29:30 +0800

    data wrangling excel

--- data_wrangling/chapter03_csv_data_01.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-path = "data_source/"
-filename = "students.csv"
-csvfile = open(path + filename, 'r')
-reader = csv.reader(csvfile)
-// print(type(reader))
-for row_list in reader:
-    print(row_list)
